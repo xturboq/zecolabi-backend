c87941745f549c813e6a62737892e8b9b1aa71eb
zecola <xyk816@gmail.com>
2024-09-29 17:50:42 +0800

    add ChartConstant

---
+++ src/main/java/com/zecola/project/constant/ChartConstant.java
@@ -0,0 +1,9 @@
+package com.zecola.project.constant;
+
+public interface ChartConstant {
+    String CHART_WAIT = "wait";
+    String CHART_RUNNING = "running";
+    String CHART_SUCCESS = "success";
+    String CHART_FAILED = "failed";
+
+}
