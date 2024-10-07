93742a37e47faab3a4196e06136db6d8fe9addd3
zecola <xyk816@gmail.com>
2024-10-07 14:59:54 +0800

    update ChartConstant

--- src/main/java/com/zecola/project/constant/ChartConstant.java
+++ src/main/java/com/zecola/project/constant/ChartConstant.java
@@ -3,7 +3,7 @@ package com.zecola.project.constant;
 public interface ChartConstant {
     String CHART_WAIT = "wait";
     String CHART_RUNNING = "running";
-    String CHART_SUCCESS = "success";
+    String CHART_SUCCEED = "succeed";
     String CHART_FAILED = "failed";
 
 }
